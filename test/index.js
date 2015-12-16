31e002d38c9f3d268b3815830db3ef4349e6626f
Nino Ulsamer <nino@divvit.com>
2015-12-16 17:28:14 +0700

    added more tests for user agents

--- test/index.js
+++ test/index.js
@@ -3,8 +3,43 @@ var should = require('chai').should(),
    getDeviceType = userAgentMapper.getDeviceType;
 
 describe('#getDeviceType', function() {
-   it('detects Chrome browser as desktop', function() {
+   it('detects Chrome as desktop', function() {
       getDeviceType('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36')
          .should.equal('desktop');
+   });
+
+   it('detects IE as desktop', function() {
+      getDeviceType('Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko')
+         .should.equal('desktop');
+   });
+
+   it ('detects iPad as tablet', function() {
+      getDeviceType('Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1')
+         .should.equal('tablet');
+   });
+
+   it('detects Android as mobile', function() {
+      getDeviceType('Mozilla/5.0 (Linux; U; Android 4.0.3; de-ch; HTC Sensation Build/IML74K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30')
+         .should.equal('mobile');
+   });
+
+   it('detects Blackberry as mobile', function() {
+      getDeviceType('Mozilla/5.0 (BlackBerry; U; BlackBerry 9850; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.254 Mobile Safari/534.11+')
+         .should.equal('mobile');
+   });
+
+   it('detects iPhone OS 4.0 as mobile', function() {
+      getDeviceType('Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7')
+         .should.equal('mobile');
+   });
+
+   it('detects iPhone OS 6.0 as mobile', function() {
+      getDeviceType('Mozilla/5.0 (iPhone; CPU iPhone OS 6_1_4 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10B350 Safari/8536.25')
+         .should.equal('mobile');
+   });
+
+   it('detects Android tablet as tablet', function() {
+      getDeviceType('mozilla/5.0_(linux;_u;_android_3.1;_en-us;_gt-p7510_build/hmj37)_applewebkit/534.13_(khtml,_like_gecko)_version/4.0_safari/534.13')
+         .should.equal('tablet');
    });
 });
